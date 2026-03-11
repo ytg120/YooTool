f61d1e1da8ec6da24b43f0284a5355fc248f520c
Tgy <yutaegyun54@gmail.com>
2026-03-11 23:30:52 +0900

    beta

--- engine/engineexec.py
+++ engine/engineexec.py
@@ -1,6 +1,16 @@
+import pygame, time, sys, os
+
+def path_getter(filename):
+    if getattr(sys, 'frozen', False):
+
+        base_path = os.path.dirname(sys.executable)
+    else:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+    
+    return os.path.join(base_path, filename)
+
 def main(jsondata):
-    import pygame
-    global clock, name, width, height, filedata, gamedata, colorlist
+    global clock, name, width, height, filedata, gamedata, colorlist, displaysurf, bg_color, color, keys, text
     gamedata = {}
     filedata = jsondata
 
@@ -23,6 +33,8 @@ def main(jsondata):
     pygame.init()
     pygame.display.set_caption(name)
     displaysurf = pygame.display.set_mode((int(width), int(height)), 0, 32)
+    keys = pygame.key.get_pressed()
+    text = pygame.font.Font(path_getter(os.path.join('data', 'Font.ttf')), 70)
 
     # setting the bg color
     bg_color = filedata['bg']
@@ -31,12 +43,33 @@ def main(jsondata):
     else:
         color = (255, 255, 255)
 
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        displaysurf.fill(color)
+        pygame.display.update()
+        clock.tick(60)
+
 class Sprites:
     def __init__(self, type, name):
         self.type = type
         self.name = name
         if self.type == 'image':
-            self.data = gamedata[self.name]['text']
-            self.imgrect = gamedata[self.name]['imgrect']
-    def set_xy(self, x, y):
+            self.data = pygame.image.load(path_getter(os.path.join('data', str(filedata['sprites'][self.name]['data']))))
+            self.rect = self.data.get_rect()
+            self.rect.center = (width/2, height/2)
+        elif self.type == 'text':
+            self.data = text.render(filedata['sprites'][self.name]['data'], True, colorlist['black'])
+            self.rect = self.data.get_rect()
+            self.rect.center = (width/2, height/2)
+        displaysurf.blit(self.data, self.rect)
 
+    def set_xy(self, x, y):
+        self.rect.center = (width/10*x, height/10*y)
+
+    def if_key_pressed(self, key):
+        return keys[key]
